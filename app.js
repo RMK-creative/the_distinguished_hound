feeac05a886972a1af5c4422d6b3bc62f27e4394
Rachael Kunz <rmk@Rachaels-MacBook-Air.local>
2021-09-04 17:22:59 +0200

    add function to close modal on scroll or click on item

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const hamburger = document.querySelector(".toggle-box");
 const modal = document.querySelector(".navbar__modal");
 
+// open & close menu
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
   modal.classList.toggle("active");
 });
+
+// menu to close when user scrolls
+document.onscroll = (e) => {
+    if (e.target.id !== "toggle-box" && e.target.id !== "navbar__modal") {
+      hamburger.classList.remove("open");
+      modal.classList.remove("active");
+    }
+  };
