7dd4ad903d3a3c6a773647b877a451ca72a4a05c
Rachael Kunz <rmk@Rachaels-MacBook-Air.local>
2021-09-07 16:23:35 +0200

    disable close menu onscroll

--- app.js
+++ app.js
@@ -8,9 +8,23 @@ hamburger.addEventListener("click", () => {
 });
 
 // menu to close when user scrolls
-document.onscroll = (e) => {
-    if (e.target.id !== "toggle-box" && e.target.id !== "navbar__modal") {
-      hamburger.classList.remove("open");
-      modal.classList.remove("active");
-    }
-  };
+// document.onscroll = (e) => {
+//     if (e.target.id !== "toggle-box" && e.target.id !== "navbar__modal") {
+//       hamburger.classList.remove("open");
+//       modal.classList.remove("active");
+//     }
+//   };
+
+
+  // sticky nav
+// const navbar = document.querySelector('.navbar');
+// let sticky = navbar.offsetTop;
+// const navbarScroll = () => {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add('sticky')
+//   } else {
+//     navbar.classList.remove('sticky');
+//   }
+// };
+
+// window.onscroll = navbarScroll;
