1504c5a1cbe614404bcb1604c38e8f750777154c
Rachael Kunz <rachaelmkunz@gmail.com>
2021-09-08 13:52:24 +0200

    remove old code & add close menu on click anywhere

--- app.js
+++ app.js
@@ -1,30 +1,16 @@
 const hamburger = document.querySelector(".toggle-box");
 const modal = document.querySelector(".navbar__modal");
 
-// open & close menu
+// open & close menu using toggle
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
   modal.classList.toggle("active");
 });
 
-// menu to close when user scrolls
-// document.onscroll = (e) => {
-//     if (e.target.id !== "toggle-box" && e.target.id !== "navbar__modal") {
-//       hamburger.classList.remove("open");
-//       modal.classList.remove("active");
-//     }
-//   };
+// close menu when clicked anywhere
+document.addEventListener("click", () => {
+  if (modal.classList.contains("active")) {
+    modal.classList.remove("active")
+     }
+  }, true);
 
-
-  // sticky nav
-// const navbar = document.querySelector('.navbar');
-// let sticky = navbar.offsetTop;
-// const navbarScroll = () => {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add('sticky')
-//   } else {
-//     navbar.classList.remove('sticky');
-//   }
-// };
-
-// window.onscroll = navbarScroll;
