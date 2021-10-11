58d7d0f850eb15cc4393b66d84adaf6e8fc5a1e2
Rachael Kunz <rachaelmkunz@gmail.com>
2021-10-11 17:29:04 +0200

    fix menu bug & prevent scroll when active

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ const modal = document.querySelector(".navbar__modal");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
   modal.classList.toggle("active");
+  // prevent scroll when menu is open
+  if (modal.classList.contains("active")) {
+    document.body.style.overflow = "hidden";
+  }
 });
 
 // close menu when clicked anywhere
-document.addEventListener("click", () => {
-  if (modal.classList.contains("active")) {
-    modal.classList.remove("active")
-     }
-  }, true);
-
+modal.addEventListener(
+  "click",
+  () => {
+    // allow scroll after link is clicked
+    document.body.style.overflow = "auto";
+    if (modal.classList.contains("active")) {
+      modal.classList.remove("active");
+      hamburger.classList.toggle("open");
+    }
+  },
+  true
+);
